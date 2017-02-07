1c67afefbd0046dc9183e30a455eb19f65eac2e8
Amin Azarbadegan <amin@azarbadegan.net>
2017-02-07 18:04:10 +0330

    initial commit

---
+++ src/app/rest.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, URLSearchParams } from "@angular/http";
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class RestService {
+  constructor(private http: Http) {
+  }
+
+  insert(table, values) {
+    return this.http.put('/api/' + table, values).map((data: Response) => data.json());
+  }
+
+  get(table) {
+    return this.http.get('/api/' + table).map((data: Response) => data.json());
+  };
+
+  getWithParams(table, values) {
+    let params: URLSearchParams = new URLSearchParams();
+    for (var key in values)
+      if (values.hasOwnProperty(key))
+        params.set(key, values[key]);
+
+    return this.http.get('/api/' + table, {search: params}).map((data: Response) => data.json());
+  }
+
+  delete(table, id) {
+    return this.http.delete('/api/' + table + '/' + id);
+  }
+
+  update(table, id, values) {
+    return this.http.post('/api/' + table + '/' + id, values)
+  }
+}
